5654b913d71dbd45c93546bcfbb192ac4a4eab5f
Nhu Tran <mohamat167@yahoo.com.vn>
2026-05-22 10:20:40 +0700

    Update Platform

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -12,11 +12,8 @@ echo "2. Grafana    → http://localhost:3000"
 echo "3. Prometheus → http://localhost:9090"
 echo ""
 
-kubectl port-forward svc/argocd-server \
-  -n argocd 8081:443 &
+kubectl port-forward svc/argocd-server -n argocd 8081:443 &
 
-kubectl port-forward svc/monitoring-grafana \
-  -n monitoring 3000:80 &
+kubectl port-forward svc/monitoring-grafana -n monitoring 3000:80 &
 
-kubectl port-forward svc/monitoring-kube-prometheus-prometheus \
-  -n monitoring 9090:9090 &
+kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 9090:9090 &
