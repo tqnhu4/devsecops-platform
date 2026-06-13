f30715d3ac982d96a1beebc89b813d211f8b3a13
Nhu Tran <mohamat167@yahoo.com.vn>
2026-06-13 11:47:08 +0700

    Update Platform

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -21,6 +21,9 @@ kubectl port-forward svc/kube-prometheus-stack-grafana -n monitoring 3000:80 &
 
 kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 9090:9090 &
 ->
-kubectl port-forward svc/kube-prometheus-stack-grafana -n monitoring 9090:9090 &
+kubectl port-forward svc/monitoring-prometheus -n monitoring 9090:9090 &
+
+
+Post "http://monitoring-kube-prometheus-prometheus:9090/api/v1/query_range": dial tcp: lookup monitoring-kube-prometheus-prometheus on 10.43.0.10:53: no such host, Post "http://monitoring-kube-prometheus-prometheus:9090/api/v1/query_range": dial tcp: lookup monitoring-kube-prometheus-prometheus on 10.43.0.10:53: no such host
 
 
